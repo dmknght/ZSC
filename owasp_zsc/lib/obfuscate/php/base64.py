c4cbc6f3a01a8940b9440e140a94adcd656d80ad
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-25 18:09:48 +0700

    Rewrite structure for base64 of php

--- owasp_zsc/lib/obfuscate/php/base64.py
+++ owasp_zsc/lib/obfuscate/php/base64.py
@@ -9,68 +9,32 @@ https://groups.google.com/d/forum/owasp-zsc [ owasp_zsc[at]googlegroups[dot]com 
 import binascii
 import random
 import string
-from owasp_zsc.cores.compatible import version
-from owasp_zsc.cores.alert import *
-from owasp_zsc.cores.get_input import _input
-_version = version()
 
 
 def encode(f):
-	val_name = ''.join(
-		random.choice(string.ascii_lowercase + string.ascii_uppercase)
-		for i in range(50))
-	data = ''
-	eval = '$' + val_name
-	if _version == 2:
-		data = '$' + val_name + ' = "' + str(binascii.b2a_base64(f)).replace(
-			'\n', '') + '";\n'
+	val_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+	eval_text = '$' + val_name
+	data = '$' + val_name + ' = "' + str(binascii.b2a_base64(f.encode(
+		'latin-1')).decode('latin-1').replace('\n', '')) + '";\n'
 
-	if _version == 3:
-		data = '$' + val_name + ' = "' + str(binascii.b2a_base64(f.encode(
-			'latin-1')).decode('latin-1').replace('\n', '')) + '";\n'
+	var_str = '$' + ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+	var_data = '$' + ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+	func_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+	func_argv = '$' + ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+	f = f"{data}\n"
+	f += f"function {func_name}({func_argv}) " + "{\n"
+	f += f"	{var_str} = base64_decode({func_argv});\n"
+	f += f"	return {var_str};\n"
+	f += "}\n"
+	f += f"{var_data} = {eval_text};\n"
+	f += f"eval({func_name}({var_data}));"
+	f += "?>\n"
 
-	var_str = '$' + ''.join(
-		random.choice(string.ascii_lowercase + string.ascii_uppercase)
-		for i in range(50))
-	var_data = '$' + ''.join(
-		random.choice(string.ascii_lowercase + string.ascii_uppercase)
-		for i in range(50))
-	func_name = ''.join(
-		random.choice(string.ascii_lowercase + string.ascii_uppercase)
-		for i in range(50))
-	func_argv = '$' + ''.join(
-		random.choice(string.ascii_lowercase + string.ascii_uppercase)
-		for i in range(50))
-	f = '''
-%s
-function %s(%s) {
-	%s = base64_decode(%s);
-	return %s;
-}
-%s = %s;
-eval(%s(%s));
-?>''' % (data, func_name, func_argv, var_str, func_argv, var_str, var_data,
-		 eval, func_name, var_data)
 	return f
 
 
-def start(content,cli):
+def start(content):
 	if '<?' in content or '?>' in content or '<?php' in content:
-		warn(
-			'We\'ve detected <? or ?> or <?php in your php code which if they wasn\'t comment, eval() will not work! so we suggest you to delete them.\n')
-		if cli == False:
-			answer = _input(
-				'Would you let me to delete php tags for you [yes/no]? ', 'any',
-				True)
-		if cli == True:
-			answer = 'y'
-			write('Would you let me to delete php tags for you [yes/no]? yes\n')
-		if answer == 'yes' or answer == 'y':
-			content = content.replace('<?php', '').replace('<?', '').replace(
-				'?>', '')
-		elif answer == 'no' or answer == 'n':
-			pass
-		else:
-			warn('You had to answer with yes or no, We count that as "no"\n')
+		content = content.replace('<?php', '').replace('<?', '').replace('?>', '')
 	return str(str('<?php \n/*\n') + str(content.replace('*/', '*_/')) + str(
 		'\n*/') + str(encode(content)) + str('\n'))
