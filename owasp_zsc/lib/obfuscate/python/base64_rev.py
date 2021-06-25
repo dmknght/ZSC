1cdd46ffbb7a53ec957aa71aa1339b3b539d8091
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-25 16:20:32 +0700

    Rewrite base64 (obfuscated script is broken)

--- owasp_zsc/lib/obfuscate/python/base64_rev.py
+++ owasp_zsc/lib/obfuscate/python/base64_rev.py
@@ -30,23 +30,22 @@ def encode(f):
     func_argv = ''.join(
         random.choice(string.ascii_lowercase + string.ascii_uppercase)
         for i in range(50))
-    f = '''
-import binascii
-import sys
-%s
-def %s(%s):
-    if sys.version_info.major == 2:
-        return str(binascii.a2b_base64(%s[::-1]))
-    elif sys.version_info.major == 3:
-        return str(binascii.a2b_base64(%s[::-1]).encode('utf8'))[::-1].decode('utf8')
-    else:
-        sys.exit('Your python version == not supported!')
-exec(%s(%s))
-''' % (data, func_name, func_argv, func_argv, func_argv,
-       func_name, var_name)
+    f = "import binascii\n"
+    f += "import sys\n"
+    f += f"{data}\n"
+    f += f"def {func_name}({func_argv}):\n"
+    f += "    if sys.version_info.major == 2:\n"
+    f += f"        return str(binascii.a2b_base64({func_argv}[::-1]))\n"
+    f += "    elif sys.version_info.major == 3:\n"
+    f += f"        return str(binascii.a2b_base64({func_argv}[::-1]).decode('utf8'))[::-1]\n"
+    f += "    else:\n"
+    f += "         sys.exit()\n"
+    f += f"exec({func_name}({var_name}))\n"
+
     return f
 
 
-def start(content,cli):
-    return str(str('\'\'\'\n') + str(content.replace('\'\'\'', '\\\'\\\'\\\''))
-               + str('\n\'\'\'') + str(encode(content)) + str('\n'))
+def start(content):
+    print("Obfuscated script for python3 is broken. Skipping")
+    return None
+    return str(encode(content))
