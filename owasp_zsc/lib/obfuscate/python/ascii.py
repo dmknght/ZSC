77f26b7b9095093fa16e485728a8b99034c41b5f
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-27 22:18:44 +0700

    Multiple times encoding with ascii for python

--- owasp_zsc/lib/obfuscate/python/ascii.py
+++ owasp_zsc/lib/obfuscate/python/ascii.py
@@ -10,24 +10,66 @@ import random
 import string
 
 
-def encode(f):
+def encode(data, times):
+    # We try to parse the code from script. We skip comments and split import to other code
+    requires_import = ["import sys"]
+
+    code_import, code_orig = "", ""
+    for line in data.split("\n"):
+        if line.startswith("import"):
+            code_import += line + "\n"
+        elif line.startswith("#"):
+            pass
+        else:
+            code_orig += line + "\n"
+
+    # Check if import is in the script. We don't want duplicate lines
+    for need_to_import in requires_import:
+        if need_to_import not in code_import:
+            code_import += need_to_import + "\n"
+
+    # Generate random strings
     var_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
-    ascii_data = ''.join([str(ord(i))+'*' for i in f])[:-1]
-    data = var_name + ' = "' + ascii_data + '"'
     var_counter = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
     var_str = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
     func_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
     func_argv = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+    index_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
+    tmp_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
 
-    f = f"{data}\n"
+    # Generate encoded payload
+    encoded_payload = code_orig
+    for i in range(0, times):
+        encoded_payload = ''.join([str(ord(i))+'*' for i in encoded_payload])[:-1]
+        print(f"[DEBUG] payload: {encoded_payload}")
+    finale_encoded_payload = var_name + ' = "' + encoded_payload + '"'
+
+    # Generate source code
+    f = f"{code_import}\n{finale_encoded_payload}\n"
     f += f"def {func_name}({func_argv}):\n"
-    f += f"    {var_str} = ''\n"
-    f += f"    for {var_counter} in {func_argv}.split('*'):\n"
-    f += f"        {var_str} += chr(int({var_counter}))\n"
-    f += f"    return {var_str}\n"
+    f += "    if sys.version_info.major == 2:\n"
+    f += f"        {tmp_name} = {func_argv}\n"
+    f += f"        for {index_name} in xrange(0, {times}):\n"
+    f += f"            {var_str} = ''\n"
+    f += f"            for {var_counter} in {tmp_name}.split('*'):\n"
+    f += f"                if {var_counter}:\n"
+    f += f"                    {var_str} += chr(int({var_counter}))\n"
+    f += f"            {tmp_name} = {var_str}\n"
+    f += f"        return {tmp_name}.replace('*', '')\n"
+    f += "    elif sys.version_info.major == 3:\n"
+    f += f"        {tmp_name} = {func_argv}\n"
+    f += f"        for {index_name} in range(0, {times}):\n"
+    f += f"            {var_str} = ''\n"
+    f += f"            for {var_counter} in {tmp_name}.split('*'):\n"
+    f += f"                if {var_counter}:\n"
+    f += f"                    {var_str} += chr(int({var_counter}))\n"
+    f += f"            {tmp_name} = {var_str}\n"
+    f += f"        return {tmp_name}.replace('*', '')\n"
+    f += "    else:\n"
+    f += "        sys.exit()\n"
     f += f"exec({func_name}({var_name}))\n"
     return f
 
 
-def start(content):
-    return str(encode(content))
+def start(content, times=1):
+    return str(encode(content, times))
