b03c50ce8a35cc427e3b1180161beb584eacc7de
Nong Hoang Tu <dmknght@parrotsec.org>
2021-06-28 07:31:20 +0700

    Improve code quality for rot13

--- owasp_zsc/lib/obfuscate/python/rot13.py
+++ owasp_zsc/lib/obfuscate/python/rot13.py
@@ -11,18 +11,34 @@ import string
 import codecs
 
 
-def encode(f):
-    var_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
-    rev_data = codecs.encode(f, "rot-13")
-    data = var_name + ' = """' + str(rev_data) + '"""'
+def encode(data):
+    # Imports that are needed to run our script
+    requires_import = ["import binascii", "import sys", "import codecs"]
 
+    # We try to parse the code from script. We skip comments and split import to other code
+    code_import, code_orig = "", ""
+    for line in data.split("\n"):
+        if line.startswith("import"):
+            code_import += line + "\n"
+        elif line.startswith("#"):
+            pass
+        else:
+            code_orig += line + "\n"
+    # Check if import is in the script. We don't want duplicate lines
+    for need_to_import in requires_import:
+        if need_to_import not in code_import:
+            code_import += need_to_import + "\n"
+
+    # Generate random strings
+    var_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
     func_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
     func_argv = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(50))
 
-    f = "import binascii\n"
-    f += "import sys\n"
-    f += "import codecs\n"
-    f += f"{data}\n"
+    # Generate encoded payload
+    encoded_payload = codecs.encode(code_orig, "rot-13")
+    final_encoded_payload = var_name + ' = """' + str(encoded_payload) + '"""'
+
+    f = code_import + f"\n{final_encoded_payload}\n\n"
     f += f"def {func_name}({func_argv}):\n"
     f += "    if sys.version_info.major == 2:\n"
     f += f"        return str({func_argv}.decode(\"rot13\"))\n"
@@ -35,4 +51,6 @@ def encode(f):
 
 
 def start(content, times):
+    if times != 1:
+        print("rot13 encoder only supports 1 time only")
     return str(encode(content))
