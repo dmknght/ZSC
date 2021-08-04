7ebeb7a187d63b08c1e3e707d1e3601c5dd3dc03
Nong Hoang Tu <dmknght@parrotsec.org>
2021-08-04 16:44:07 +0700

    improve code format

--- owasp_zsc/modules/encoders/linux_x86/chmod/xor_random.py
+++ owasp_zsc/modules/encoders/linux_x86/chmod/xor_random.py
@@ -17,8 +17,7 @@ class Encoder(BaseModule):
     def encode(self, shellcode):
         eax = str('0x0f')
         while True:
-            eax_1 = binascii.b2a_hex((''.join(random.choice(chars) for i in range(1))).encode('latin-1')).decode(
-                'latin-1')
+            eax_1 = binascii.b2a_hex((''.join(random.choice(chars) for i in range(1))).encode('latin-1')).decode('latin-1')
             eax_1 = str('0') + str(eax_1[1])
             eax_2 = "%x" % (int(eax, 16) ^ int(eax_1, 16))
             if eax > eax_1:
@@ -30,8 +29,7 @@ class Encoder(BaseModule):
         ecx = str(shellcode.rsplit('\n')[8])
         ecx_value = str(shellcode.rsplit('\n')[8].rsplit()[1][1:])
         while True:
-            ecx_1 = binascii.b2a_hex((''.join(random.choice(chars) for i in range(4))).encode('latin-1')).decode(
-                'latin-1')
+            ecx_1 = binascii.b2a_hex((''.join(random.choice(chars) for i in range(4))).encode('latin-1')).decode('latin-1')
             ecx_2 = "%x" % (int(ecx_value, 16) ^ int(ecx_1, 16))
             if '00' not in str(ecx_1) and '00' not in str(ecx_2) and len(ecx_1) >= 7 and len(ecx_2) >= 7:
                 break
@@ -61,11 +59,9 @@ class Encoder(BaseModule):
             while True:
                 if 'push $0x' in char:
                     ebx = char.rsplit()[1][1:]
-                    ebx_1 = binascii.b2a_hex(
-                        (''.join(random.choice(chars) for i in range(4))).encode('latin-1')).decode('latin-1')
+                    ebx_1 = binascii.b2a_hex((''.join(random.choice(chars) for i in range(4))).encode('latin-1')).decode('latin-1')
                     ebx_2 = "%x" % (int(ebx[2:], 16) ^ int(ebx_1, 16))
-                    if '00' not in str(ebx_1) and '00' not in str(ebx_2) and len(ebx_2) >= 7 and len(
-                            ebx_1) >= 7 and '-' not in ebx_1:
+                    if '00' not in str(ebx_1) and '00' not in str(ebx_2) and len(ebx_2) >= 7 and len(ebx_1) >= 7 and '-' not in ebx_1:
                         ebx_2 = ebx_2.replace('-', '')
                         command = f'\npush $0x{str(ebx_1)}\npop %ebx\npush $0x{str(ebx_2)}\npop %edx\nxor %ebx, %edx\npush %edx\n'
                         middle = middle.replace(char, command)
